ca659f2431702e67158120263f62562335b47608
Vitória Tayná <156841547+taynaadev@users.noreply.github.com>
2026-08-25 10:57:46 -0300

    Add exercicio21.c

---
+++ Lista-Cap1/exercicio21.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // Versão 1
+    printf("Treinamento em programação.\nLinguagem C.\n");
+
+    // Versão 2
+    printf("Treinamento em programação.\n");
+    printf("Linguagem C.\n");
+
+    // Versão 3
+    printf("+-----------------------------+\n");
+    printf("| Treinamento em programação. |\n");
+    printf("| Linguagem C.                |\n");
+    printf("+-----------------------------+\n");
+
+    return 0;
+}
