2db5c1ddf371d32a614bbf0f0816252d74dbe187
Vitória Tayná <156841547+taynaadev@users.noreply.github.com>
2026-08-25 10:59:24 -0300

    Add exercicio24.c
    This program prints a formatted list of students and their grades.

---
+++ Lista-Cap1/exercicio24.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%-10s %5s\n", "ALUNO(A)", "NOTA");
+    printf("%-10s %5s\n", "=========", "=====");
+    printf("%-10s %5.1f\n", "ALINE", 9.0);
+    printf("%-10s %5s\n", "MÁRIO", "DEZ");
+    printf("%-10s %5.1f\n", "SÉRGIO", 4.5);
+    printf("%-10s %5.1f\n", "SHIRLEY", 7.0);
+
+    return 0;
+}
