2e660ef273d39651c8ad25f01c503781f760ef74
Vitória Tayná <156841547+taynaadev@users.noreply.github.com>
2026-08-25 10:37:34 -0300

    Add exercicio03.c
    This program demonstrates the use of single-line and multi-line comments.

---
+++ Lista-Cap1/exercicio03.c
@@ -0,0 +1,16 @@
+/*
+ * Esse programa demonstra o uso de comentários
+ * de múltiplas linhas e de uma única linha.
+ *
+ * Primeiro programa
+ */
+
+#include <stdio.h>  /* Biblioteca para printf() */
+
+int main()
+{
+    // Exibe uma mensagem na tela
+    printf("Primeiro programa.\n");
+
+    return 0;
+}
